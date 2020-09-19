6c648cc0e7827f5f49fb0f0d08959d706cabaf23
bendoan4366 <ben.doan4366@gmail.com>
2020-09-19 14:18:16 -0400

    adding delta functions

--- app.py
+++ app.py
@@ -3,8 +3,6 @@ from pyspark import SparkContext, SparkConf
 import os
 from read_s3_boto import get_matching_s3_objects, get_matching_s3_keys, get_s3_object_to_df, get_spark_dataframes
 
-sc = SparkContext.getOrCreate()
-
 spark = SparkSession \
   .builder \
   .appName("LoadDelta") \
@@ -13,40 +11,33 @@ spark = SparkSession \
   .config('spark.jars.packages', r'C:\Users\kndoa\DevTools\spark-3.0.1-bin-hadoop2.7\jars\aws-java-sdk-1.11.859.jar') \
   .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension") \
   .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog")\
-  .config('spark.delta.logStore.class=org.apache.spark.sql.delta.storage.S3SingleDriverLogStore') \
+  .config("spark.delta.logStore.class", "org.apache.spark.sql.delta.storage.S3SingleDriverLogStore") \
   .getOrCreate()
 
 spark.sparkContext.addPyFile(r"C:\Users\kndoa\DevTools\spark-3.0.1-bin-hadoop2.7\jars\delta-core_2.12-0.7.0.jar")
-from delta.tables import *
+conf = spark.sparkContext._conf.setAll([('spark.delta.logStore.class','org.apache.spark.sql.delta.storage.S3SingleDriverLogStore')])
+spark.sparkContext._conf.getAll()
 
 aws_access_key = os.getenv("AWS_ACCESS_KEY")
 aws_secret = os.getenv("AWS_SECRET_ACCESS_KEY_ID")
 
-sc._jsc.hadoopConfiguration().set("fs.s3n.awsAccessKeyId", aws_access_key)
-sc._jsc.hadoopConfiguration().set("fs.s3n.awsSecretAccessKey", aws_secret)
-sc._jsc.hadoopConfiguration().set("fs.s3n.endpoint", "s3.amazonaws.com")
+spark.sparkContext._jsc.hadoopConfiguration().set("fs.s3n.awsAccessKeyId", aws_access_key)
+spark.sparkContext._jsc.hadoopConfiguration().set("fs.s3n.awsSecretAccessKey", aws_secret)
+spark.sparkContext._jsc.hadoopConfiguration().set("fs.s3n.endpoint", "s3.amazonaws.com")
 
 bucket = "covid19-lake"
-print("\n" + "df_county_populations - starting...." + "\n")
+
 df_county_populations = get_spark_dataframes(spark, bucket,  "static-datasets/csv/CountyPopulation", ".csv", "csv")
+df_county_populations = df_county_populations.withColumnRenamed("Population Estimate 2018", "2018_pop_estimate")
 print("\n" + "df_county_populations - done" + "\n")
 
-print("\n" + "df_county_cases - starting...." + "\n")
 df_county_cases = get_spark_dataframes(spark, bucket, "rearc-covid-19-nyt-data-in-usa/csv/us-counties", ".csv", "csv")
 print("\n" + "df_county_cases - done...." + "\n")
 
-print("\n" + "state_testing - starting...." + "\n")
 df_state_tests = get_spark_dataframes(spark, bucket, "rearc-covid-19-testing-data/json/states_daily", ".json", "json")
 print("\n" + "state_testing - done...." + "\n")
 
-df_county_populations.printSchema()
-df_county_populations.show(10)
-
-df_county_cases.printSchema()
-df_county_cases.show(10)
-
-df_state_tests.printSchema()
-df_state_tests.show(10)
-
-
-df_county_populations.write.format("parquet").save("s3n://covid-delta-lake/test")
+from delta.tables import *
+df_county_populations.write.format("delta").mode("overwrite").save("s3n://covid-delta-lake/test/populations")
+df_state_tests.write.format("delta").mode("overwrite").save("s3n://covid-delta-lake/test/tests")
+df_county_cases.write.format("delta").mode("overwrite").save("s3n://covid-delta-lake/test/cases")
