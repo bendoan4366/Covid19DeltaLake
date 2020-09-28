bd782f6cf04edf1a46d4a80461be0a9e24867cff
bendoan4366 <ben.doan4366@gmail.com>
2020-09-28 19:27:50 -0400

    fixing s3 read error

--- scala/delta-lake-scala/src/main/scala/org/knd/App.scala
+++ scala/delta-lake-scala/src/main/scala/org/knd/App.scala
@@ -21,14 +21,23 @@ object App {
     val aws_access_key = scala.util.Properties.envOrElse("AWS_ACCESS_KEY", "notAvailable" )
     val aws_secret = scala.util.Properties.envOrElse("AWS_SECRET_ACCESS_KEY_ID", "notAvailable" )
 
-    spark.sparkContext.hadoopConfiguration.set("fs.s3a.awsAccessKeyId", aws_access_key)
-    spark.sparkContext.hadoopConfiguration.set("fs.s3a.awsSecretAccessKey", aws_secret)
+    spark.sparkContext.hadoopConfiguration.set("fs.s3a.access.key", aws_access_key)
+    spark.sparkContext.hadoopConfiguration.set("fs.s3a.secret.key", aws_secret)
     spark.sparkContext.hadoopConfiguration.set("fs.s3a.endpoint", "s3.amazonaws.com")
 
     print("\n" + "====================HERE====================" + "\n")
 
-    val data = spark.read.parquet("s3a://knd-udacity-data-lake-project/artists/artists.parquet/*.parquet")
-    data.show(10)
+    // https://covid19-lake.s3.us-east-2.amazonaws.com/rearc-covid-19-nyt-data-in-usa/json/us-counties/part-00000-333f1f52-c1f9-4870-a29f-d2f09fb8d11c-c000.json
+    val df_cases_data = spark.read.json("s3a://covid19-lake/rearc-covid-19-nyt-data-in-usa/json/us-counties/*.json")
+    val df_testing_data = spark.read.option("header", true).option("inferSchema", true).csv("s3a://covid19-lake/rearc-covid-19-testing-data/csv/states_daily/*.csv")
+    val df_predictions_data = spark.read.json("s3a://covid19-lake/rearc-covid-19-prediction-models/json/county-predictions/*.json")
+    val df_county_populations = spark.read.option("header", true).option("inferSchema", true).csv("s3a://covid19-lake/static-datasets/csv/CountyPopulation/*.csv")
+
+    df_cases_data.printSchema()
+    df_testing_data.printSchema()
+    df_predictions_data.printSchema()
+    df_county_populations.printSchema()
+
 
 
   }
