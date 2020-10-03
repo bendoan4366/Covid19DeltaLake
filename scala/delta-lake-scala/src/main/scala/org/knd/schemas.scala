b16ebe4512050620e8bfa9de6af3b072018e6552
bendoan4366 <ben.doan4366@gmail.com>
2020-10-03 17:25:05 -0400

    adding structType schemas for cases and prediction tables

--- scala/delta-lake-scala/src/main/scala/org/knd/schemas.scala
+++ scala/delta-lake-scala/src/main/scala/org/knd/schemas.scala
@@ -1,5 +1,29 @@
 package org.knd
 
-object schenas {
+import org.apache.spark.sql.types.{DateType, DoubleType, IntegerType, StringType, StructField, StructType, TimestampType}
+
+object schemas {
+
+  val cases_schema = StructType(
+    List(
+      StructField("date", DateType),
+      StructField("county", StringType),
+      StructField("state", StringType),
+      StructField("fips", StringType),
+      StructField("cases", IntegerType),
+      StructField("deaths", IntegerType),
+    )
+  )
+
+  val prediction_schema = StructType(
+    List(
+      StructField("countyfips", DateType),
+      StructField("countyname", StringType),
+      StructField("predicted_date", DateType),
+      StructField("predicted_deaths", DoubleType),
+      StructField("severity_county_5-day", DoubleType),
+      StructField("statename", StringType)
+    )
+  )
 
 }
