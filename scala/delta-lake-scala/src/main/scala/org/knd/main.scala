018077c7b874e68bee8feef43e652fabc8e805b1
bendoan4366 <ben.doan4366@gmail.com>
2020-11-23 23:45:30 -0500

    debugging config factory settings for aws

--- scala/delta-lake-scala/src/main/scala/org/knd/main.scala
+++ scala/delta-lake-scala/src/main/scala/org/knd/main.scala
@@ -1,7 +1,9 @@
 package org.knd
-import com.typesafe.config.ConfigFactory
-import scala.util.Properties
+import java.io.File
 
+import com.typesafe.config.{ConfigFactory, ConfigParseOptions}
+
+import scala.util.Properties
 import org.knd.helpers.read_tables._
 import org.apache.spark.sql.{DataFrame, SparkSession}
 import org.knd.helpers.read_tables
@@ -9,11 +11,35 @@ import org.knd.helpers.read_tables
 import scala.::
 import scala.collection.mutable
 import scala.io.Source
-import scala.reflect.io.File
 
 object main {
 
   def main(args: Array[String]) : Unit = {
+
+
+    val parseOptions = ConfigParseOptions.defaults()
+      .setAllowMissing(false)
+
+    val config = ConfigFactory.parseFile(new File("src/main/resources/application.conf"),
+      parseOptions).resolve()
+
+    print(config)
+
+    val delta_config = config.getConfig("delta")
+    val aws_config = delta_config.getConfig("aws")
+
+    val aws_access_key = aws_config.getString("aws_access_key")
+    val aws_secret = aws_config.getString("aws_secret")
+    val delta_lake_bucket = aws_config.getString("delta_lake_bucket")
+
+    print("\n" + aws_access_key + "\n")
+    print("\n" + aws_secret + "\n")
+    print("\n" + delta_lake_bucket + "\n")
+
+    /*
+    val local_aws_access_key = scala.util.Properties.envOrElse("AWS_ACCESS_KEY", "")
+    val local_aws_secret = scala.util.Properties.envOrElse("AWS_SECRET_ACCESS_KEY_ID", "")
+
 
     val spark = SparkSession
     .builder()
@@ -23,12 +49,8 @@ object main {
     .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog")
     .getOrCreate()
 
-    val aws_access_key = ConfigFactory.load("delta_properties.conf").getString("delta.aws.aws_access_key")
-    val aws_secret = ConfigFactory.load("delta_properties.conf").getString("delta.aws.aws_secret")
-    val delta_lake_bucket = ConfigFactory.load("delta_properties.conf").getString("delta.aws.url")
-
-    spark.sparkContext.hadoopConfiguration.set("fs.s3a.access.key", aws_access_key)
-    spark.sparkContext.hadoopConfiguration.set("fs.s3a.secret.key", aws_secret)
+    spark.sparkContext.hadoopConfiguration.set("fs.s3a.access.key", local_aws_access_key)
+    spark.sparkContext.hadoopConfiguration.set("fs.s3a.secret.key", local_aws_secret)
     spark.sparkContext.hadoopConfiguration.set("fs.s3a.endpoint", "s3.amazonaws.com")
 
     //read tables from various sources
@@ -51,6 +73,8 @@ object main {
     education_estimates_table.write.format("delta").mode("overwrite").save(delta_lake_bucket + "education/")
     poverty_estimates_table.write.format("delta").mode("overwrite").save(delta_lake_bucket + "poverty/")
     populations_table.write.format("delta").mode("overwrite").save(delta_lake_bucket + "populations/")
+
+     */
 
     }
 
